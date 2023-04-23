35b27bcf37732c0319c090829d00ce0ee61bdf28
Mugil99 <124775550+Mugil99@users.noreply.github.com>
2023-04-23 13:06:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+//your code here
+let tableEl = document.getElementById('myTable');
+let priceArr = Array.from(document.querySelectorAll("td[data-ns-test]"));
+// console.log(priceArr);
+let totalPrice = 0;
+for (const priceOfEachItem of priceArr) {
+  totalPrice += Number(priceOfEachItem.textContent);
+}
 
-const getSum = () => {
-//Add your code here
-  
-};
+let totalRow = document.createElement('tr');
+let newcell = totalRow.insertCell(0);
+newcell.textContent = totalPrice
+totalRow.setAttribute('data-ns-test', 'grandTotal')
 
-getSumBtn.addEventListener("click", getSum);
-
+tableEl.insertAdjacentElement('beforeend', totalRow);
